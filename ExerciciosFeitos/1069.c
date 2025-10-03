8c4c3007067039ef1084343962971ca0298bd0cc
henrique-peruzzo-menezes <henrique.peruzzo@unifesp.br>
2025-10-03 07:23:08 -0300

    fix 1069

--- ExerciciosFeitos/1069.c
+++ ExerciciosFeitos/1069.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 typedef struct {
-    char items [1000];
+    char items[1000];
     int top;
 } Stack;
 
-void initStack (Stack *s) {
-    s->top = -1;
-}
+void initStack(Stack *s) { s->top = -1; }
 
-int isEmpty (Stack *s){
-    return s->top == -1;
-}
+int isEmpty(const Stack *s) { return s->top == -1; }
 
-void add(Stack *s, char valor){
-    if (s->top < 999){
+void push(Stack *s, char valor) {
+    if (s->top < 999) {
         s->items[++(s->top)] = valor;
     }
 }
 
-char remove (Stack *s) {
+char pop(Stack *s) {
     if (!isEmpty(s)) {
         return s->items[(s->top)--];
     }
     return '\0';
 }
 
-int countDiamond (const char *sequencia){
+int countDiamond(const char *sequencia) {
     Stack s;
     initStack(&s);
     int count = 0;
 
-    for (int i = 0; i< strlen(sequencia); i++) {
+    size_t len = strlen(sequencia);
+    for (size_t i = 0; i < len; i++) {
         if (sequencia[i] == '<') {
-            add(&s, '<');
-        }
-        else if (sequencia[i] == '>') {
-            if (!isEmpty(&s)){
-                remove(&s);
+            push(&s, '<');
+        } else if (sequencia[i] == '>') {
+            if (!isEmpty(&s)) {
+                pop(&s);
                 count++;
             }
         }
@@ -47,13 +42,13 @@ int countDiamond (const char *sequencia){
     return count;
 }
 
-int main () {
+int main(void) {
     int N;
     char sequencia[1001];
-    scanf("%d", &N);
+    if (scanf("%d", &N) != 1) return 0;
 
-    while (N--){
-        scanf("%s", sequencia);
+    while (N--) {
+        scanf("%1000s", sequencia);
         printf("%d\n", countDiamond(sequencia));
     }
     return 0;
